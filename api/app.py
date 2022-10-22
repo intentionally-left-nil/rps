074adbb18711959345a49fb5c30362a91472751c
Anil Kulkarni <anil@terminal.space>
2022-10-22 15:44:28 -0700

    Add APIs to make turns in a round Each round contains a move from each player, e.g. rock/paper/ or scissors. Players create a move by calling PATCH /game/1/round/4 to add the move Players cannot move onte the next round until both answers have been submitted.

--- api/app.py
+++ api/app.py
@@ -43,7 +43,8 @@ class JSONBadRequest(BadRequest):
     'properties': {
         'user_id': {'type': 'string'},
         'name': {'type': 'string'},
-    }
+    },
+    'required': ['user_id', 'name']
 })
 def join_game(id: str):
     if join_game_as_player(id, 'p1'):
@@ -90,8 +91,56 @@ def get_game_state(id: str):
     return jsonify(result)
 
 
+@app.patch('/game/<game_id>/round/<id>')
+@expects_json({
+    'type': 'object',
+    'properties': {
+        'player_id': {'type': 'string', 'enum': ['p1', 'p2']},
+        'move': {'type': 'string', 'enum': ['rock', 'paper', 'scissors']},
+    },
+    'required': ['player_id', 'move']
+})
+def add_round(game_id: str, id: str):
+    try:
+        round = int(id)
+    except ValueError:
+        return make_error_response(400, 'bad_request', 'Tried to submit a move for an invalid round')
+    if round < 1 or round > 10_000:  # Arbitrary, prevent overflow or other abuse
+        return make_error_response(400, 'bad_request', 'Tried to submit a move for an invalid round')
+
+    current_round = max(1, bytes_to_int(r.hget(game_key(game_id), 'round')))
+
+    if round != current_round:
+        return make_error_response(400, 'wrong_round', f'We are currently on round {current_round}, not {round}')
+
+    if r.hsetnx(game_key(game_id), round_key(round, g.data['player_id']), g.data['move']) == 0:
+        return make_error_response(400, 'already_played', f'The player already submitted a move for round {round}')
+
+    moves = r.hmget(game_key(game_id), round_key(round, 'p1'), round_key(round, 'p2'))
+    moves = [move.decode('utf-8') if move else None for move in moves]
+    if moves and moves[0] and moves[1]:
+        # Both players have submitted their result, advance to the next round
+        r.hset(game_key(game_id), 'round', round + 1)
+        if did_win((moves[0]), moves[1]):
+            r.hincrby(game_key(game_id), 'p1_score', 1)
+        elif did_win(moves[1], moves[0]):
+            r.hincrby(game_key(game_id), 'p2_score', 1)
+    return jsonify({'ok': True})
+
+
+@app.get('/game/<game_id>/round/<id>')
+def get_round(game_id: str, id: str):
+    try:
+        round = int(id)
+    except ValueError:
+        return make_error_response(400, 'bad_request', 'Invalid round')
+    moves = r.hmget(game_key(game_id), round_key(round, 'p1'), round_key(round, 'p2'))
+    moves = [move.decode('utf-8') if move else None for move in moves]
+    return jsonify(dict(zip(['p1', 'p2'], moves)))
+
+
 # snippet from https://pypi.org/project/flask-expects-json
-@app.errorhandler(400)
+@ app.errorhandler(400)
 def bad_request(error):
     if isinstance(error.description, ValidationError):
         original_error = error.description
@@ -103,6 +152,10 @@ def bad_request(error):
 
 def game_key(game_id: str) -> str:
     return f'game:{game_id}'
+
+
+def round_key(round: int, player_id: str) -> str:
+    return f'round_{round}:{player_id}'
 
 
 def make_error_response(code: int, type: str, message: str) -> Response:
@@ -125,8 +178,16 @@ def bytes_to_int(b: (bytes | None)) -> int:
         return 0
 
 
+def did_win(move: str, other_move: str) -> bool:
+    winning_conditions = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper'
+    }
+    return winning_conditions[move] == other_move
+
+
 def init():
-    global r
     # TODO: debug/production configuration based on runtime flags
     # snippet from https://stackoverflow.com/questions/39451002/ensure-the-post-data-is-valid-json
 
