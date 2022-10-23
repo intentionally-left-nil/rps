a67ecd6af1852cbd6f4e952f14cfb55bedc53e9a
Anil Kulkarni <anil@terminal.space>
2022-10-22 23:23:30 -0700

    Implement basic homepage Create ahomepage which just does a URL redirect to a random game URL when clicked

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Button from 'react-bootstrap/Button';
+import { generateSlug } from 'random-word-slugs';
 
 const Home: React.FC = () => {
-  return <div>Welcome home</div>;
+  const onClick = useCallback(() => {
+    window.location.href = `/game/${generateSlug(4)}`;
+  }, []);
+  return (
+    <div>
+      <h1>Rock, Paper, Scissors</h1>
+      <Button onClick={onClick}>Create a game</Button>
+    </div>
+  );
 };
 
 export default Home;
