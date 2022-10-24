5c3dd3106da1b913e9ba0e738b5c562f6801bbee
Anil Kulkarni <anil@terminal.space>
2022-10-24 10:14:40 -0700

    Join the game when entering the lobby

--- web/src/Game.tsx
+++ web/src/Game.tsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import PlayerName from './PlayerName';
-import { useLocalStorage } from './util';
+import { useBackend, useLocalStorage } from './util';
 import InvitePlayer from './Invite';
+import { v4 as uuid4 } from 'uuid';
 
 const Game: React.FC<{ id: string }> = ({ id }) => {
   const [name, setName] = useLocalStorage('name', 'Player 1');
+  const [userId] = useLocalStorage('user_id', uuid4());
+  const [playerId, setPlayerId] = useState<string>();
   const [opponentName, setOpponentName] = useState();
+  const playerData = useMemo(() => {
+    return {
+      user_id: userId,
+      name,
+    };
+  }, [name, userId]);
+
+  const onJoinGame = useCallback<(data: { player_id: string }) => void>(
+    ({ player_id }) => {
+      setPlayerId(player_id);
+    },
+    []
+  );
+
+  useBackend(`game/${id}`, 'POST', playerData, onJoinGame);
 
   return (
     <Container fluid className="pt-3">
--- web/src/util.tsx
+++ web/src/util.tsx
@@ -18,3 +18,45 @@ export function useLocalStorage<T>(
   }, [key, state]);
   return [state, setState];
 }
+
+export function useBackend<D, T>(
+  path: string,
+  method: string,
+  data: D | void,
+  onSuccess: (data: T) => void
+) {
+  useEffect(() => {
+    if (data == null) {
+      return;
+    }
+
+    let url = `http://localhost:5000/${path}`;
+    let body = null;
+    switch (method.toUpperCase()) {
+      case 'GET':
+        url += `?` + new URLSearchParams(data).toString();
+        break;
+      case 'PUT':
+      case 'PATCH':
+      case 'POST':
+        body = JSON.stringify(data);
+        break;
+    }
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+    fetch(url, { method, headers, body })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json().then((result) => onSuccess(result));
+        } else {
+          // TODO handle errors
+          console.log(resp);
+        }
+      })
+      .catch((resp) => {
+        // TODO handle errors
+        console.log(resp);
+      });
+  }, [data, method, onSuccess, path]);
+}
